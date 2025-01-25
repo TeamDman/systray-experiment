2d16600bdc1a9d6daa98d442bbc033aa9fc57d10
TeamDman <TeamDman9201@gmail.com>
2025-01-25 16:35:53 -0500

    builds but doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-// #![windows_subsystem = "windows"]  // Hide the console window on Windows
+// #![windows_subsystem = "windows"]
 
 use clap::Parser;
+use std::{thread, time::Duration};
 use tracing::{info, Level};
 use tracing_subscriber::EnvFilter;
+use windows::Win32::Foundation::HWND;
+use windows::Win32::System::Console::GetConsoleWindow;
+use windows::Win32::UI::WindowsAndMessaging::{ShowWindow, SW_HIDE, SW_SHOW};
 
 /// Command-line arguments
 #[derive(Parser, Debug)]
@@ -13,9 +17,19 @@ struct Args {
     debug: bool,
 }
 
+/// Toggles the visibility of the console window
+fn toggle_console_visibility(show: bool) {
+    unsafe {
+        let hwnd: HWND = GetConsoleWindow();
+        if hwnd.0.is_null() {
+            ShowWindow(hwnd, if show { SW_SHOW } else { SW_HIDE }).expect("should show window");
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     color_eyre::install()?;
-    
+
     let args = Args::parse();
 
     // Setup logging
@@ -29,13 +43,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .from_env_lossy();
     tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
+    info!("Starting the application...");
 
+    let mut show_console = true;
+    loop {
+        // Toggle console visibility
+        toggle_console_visibility(show_console);
+        show_console = !show_console;
 
-    info!("Hi!"); 
-    println!("Hi!"); 
-    // read line
-    println!("Hit enter to close...");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input)?;
-    Ok(())
+        // Log the state
+        if show_console {
+            info!("Console is now visible.");
+        } else {
+            info!("Console is now hidden.");
+        }
+
+        // Wait for 3 seconds
+        thread::sleep(Duration::from_secs(3));
+    }
 }
